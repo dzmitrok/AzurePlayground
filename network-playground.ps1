d9e0fea0845d595b2e5b17b98e3a7abab2638a8b
dzmitrok <dzmitrok@users.noreply.github.com>
2020-03-16 17:05:37 +0300

    eod commit

--- network-playground.ps1
+++ network-playground.ps1
@@ -1,29 +1,75 @@
-﻿$NorthVMSubnetName = 'NorthVMSubnet'
+﻿$GWSharedKey = 'Tomato$4'
+
+$NorthVMSubnetName = 'NorthVMSubnet'
 $NorthVNetName = 'NorthVNet'
 $NorthGWPubIPName = 'North_GW_Public_IP'
+$NorthVMPubIPName = 'North_VM_Public_IP'
 $NorthGWConfigName = 'NorthGWConfig'
 $NorthGWName = 'NorthGW'
 $NorthGWConnectionName = 'NorthToWest'
+$NorthNSGRuleSSHName = 'NorthNSGRuleSSH'
+$NorthVMName = 'NorthVM'
+#Create VM VNet  Config
 $NorthVMSubnet = New-AzVirtualNetworkSubnetConfig -Name $NorthVMSubnetName -AddressPrefix 10.10.10.0/26
+#Create VNet Gateway Config
 $NorthGWSubnet = New-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix 10.10.10.64/26
+#Create VNet
 $NorthVNet = New-AzVirtualNetwork -ResourceGroupName North -Name $NorthVNetName -Location NorthEurope -AddressPrefix 10.10.10.0/25 -Subnet $NorthVMSubnet,$NorthGWSubnet
+# Grab VNet conif
 $NorthVNSubnet = Get-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $NorthVNet
+# Create a VNet Gateway Public IP Address
 $NorthGWPubIP = New-AzPublicIpAddress -Name $NorthGWPubIPName -ResourceGroupName North -Location NorthEurope -AllocationMethod Dynamic
+# Create a VNet Gateway Config
 $NorthGWConfig = New-AzVirtualNetworkGatewayIpConfig -Name $NorthGWConfigName -SubnetId $NorthVNSubnet.Id -PublicIpAddressId $NorthGWPubIP.Id
-New-AzVirtualNetworkGateway -Name $NorthGWName -ResourceGroupName North -Location NorthEurope -IpConfigurations $NorthGWConfig  -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic
-New-AzVirtualNetworkGatewayConnection -Name $NorthGWConnectionName -ResourceGroupName North -Location NorthEurope -VirtualNetworkGateway1 $NorthGW -VirtualNetworkGateway2 $WestGW -ConnectionType Vnet2Vnet -SharedKey 'Tomato$4'
+# Create a VNet Gateway
+$NorthGW = New-AzVirtualNetworkGateway -Name $NorthGWName -ResourceGroupName North -Location NorthEurope -IpConfigurations $NorthGWConfig  -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic
+
+# Create a public IP address 
+$NorthVMPubIP = New-AzPublicIpAddress -Name $NorthVMPubIPName -ResourceGroupName North -Location NorthEurope -AllocationMethod Dynamic
+# Create an inbound network security group rule for port 22
+$NorthNSGRuleSSH = New-AzNetworkSecurityRuleConfig -Name $NorthNSGRuleSSHName -Description "North VM port 22 Network security rule" `
+-Protocol Tcp -Direction Inbound  -SourceAddressPrefix * -SourcePortRange * -DestinationPortRange 22  -DestinationAddressPrefix * -Access Allow -Priority 1000
+# Create a network security group
+$NorthNSG = New-AzNetworkSecurityGroup -ResourceGroupName North -Location NorthEurope -Name $NorthNSGRuleSSHName -SecurityRules $NorthNSGRuleSSH
+$NorthVMNic = New-AzNetworkInterface -Name $($NorthVMName+"Nic") -ResourceGroupName North -Location NorthEurope -SubnetId $NorthVNet.Subnets[0].id -PublicIpAddressId $NorthVMPubIP.Id -NetworkSecurityGroupId $NorthNSG.Id
+
+
+
+
 
 $WestVMSubnetName = 'WestVMSubnet'
 $WestVNetName = 'WestVNet'
 $WestGWPubIPName = 'West_GW_Public_IP'
+$WestVMPubIPName = 'West_VM_Public_IP'
 $WestGWConfigName = 'WestGWConfig'
 $WestGWName = 'WestGW'
 $WestGWConnectionName = 'WestToNorth'
+$WestNSGRuleSSHName = 'WestNSGRuleSSH'
+$WestVMName = 'WestVM'
+#Create VM VNet  Config
 $WestVMSubnet = New-AzVirtualNetworkSubnetConfig -Name $WestVMSubnetName -AddressPrefix 10.10.10.128/26
+#Create VNet Gateway Config
 $WestGWSubnet = New-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix 10.10.10.192/26
+#Create VNet
 $WestVNet = New-AzVirtualNetwork -ResourceGroupName West -Name $WestVNetName -Location WestEurope -AddressPrefix 10.10.10.128/25 -Subnet $WestVMSubnet,$WestGWSubnet
+# Grab VNet conif
 $WestVNSubnet = Get-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $WestVNet
+# Create a VNet Gateway Public IP Address
 $WestGWPubIP = New-AzPublicIpAddress -Name $WestGWPubIPName -ResourceGroupName West -Location WestEurope -AllocationMethod Dynamic
+# Create a VNet Gateway Config
 $WestGWConfig = New-AzVirtualNetworkGatewayIpConfig -Name $WestGWConfigName -SubnetId $WestVNSubnet.Id -PublicIpAddressId $WestGWPubIP.Id
-New-AzVirtualNetworkGateway -Name $WestGWName -ResourceGroupName West -Location WestEurope -IpConfigurations $WestGWConfig  -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic
-New-AzVirtualNetworkGatewayConnection -Name $WestGWConnectionName -ResourceGroupName West -Location WestEurope -VirtualNetworkGateway1 $WestGW -VirtualNetworkGateway2 $NorthGW -ConnectionType Vnet2Vnet -SharedKey 'Tomato$4'
+# Create a VNet Gateway
+$WestGW = New-AzVirtualNetworkGateway -Name $WestGWName -ResourceGroupName West -Location WestEurope -IpConfigurations $WestGWConfig  -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic
+
+
+New-AzVirtualNetworkGatewayConnection -Name $WestGWConnectionName -ResourceGroupName West -Location WestEurope -VirtualNetworkGateway1 $WestGW -VirtualNetworkGateway2 $NorthGW -ConnectionType Vnet2Vnet -SharedKey $GWSharedKey
+New-AzVirtualNetworkGatewayConnection -Name $NorthGWConnectionName -ResourceGroupName North -Location NorthEurope -VirtualNetworkGateway1 $NorthGW -VirtualNetworkGateway2 $WestGW -ConnectionType Vnet2Vnet -SharedKey $GWSharedKey
+
+# Create a public IP address 
+$WestVMPubIP = New-AzPublicIpAddress -Name $WestVMPubIPName -ResourceGroupName West -Location WestEurope -AllocationMethod Dynamic
+# Create an inbound network security group rule for port 22
+$WestNSGRuleSSH = New-AzNetworkSecurityRuleConfig -Name $WestNSGRuleSSHName -Description "West VM port 22 Network security rule" `
+-Protocol Tcp -Direction Inbound  -SourceAddressPrefix * -SourcePortRange * -DestinationPortRange 22  -DestinationAddressPrefix * -Access Allow -Priority 1000
+# Create a network security group
+$WestNSG = New-AzNetworkSecurityGroup -ResourceGroupName West -Location WestEurope -Name $WestNSGRuleSSHName -SecurityRules $WestNSGRuleSSH
+$WestVMNic = New-AzNetworkInterface -Name $($WestVMName+"Nic") -ResourceGroupName West -Location WestEurope -SubnetId $WestVNet.Subnets[0].id -PublicIpAddressId $WestVMPubIP.Id -NetworkSecurityGroupId $WestNSG.Id
